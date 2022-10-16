0dbe49ef768cf6ecc51f98d7b2ea98d03c12c58f
psakowski <p.sakowski1213@gmail.com>
2022-10-16 22:20:52 +0200

    first commit

---
+++ src/components/List/List.js
@@ -0,0 +1,10 @@
+const List = () => {
+    return (
+        <header>
+            <h1>Things to do<span>soon</span></h1>
+            <p>Interesting things I want to check out!</p>
+        </header>
+    );
+};
+
+export default List;
