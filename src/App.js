deb523f566960504245f7e0e57fcabdc2fdc0f39
psakowski <p.sakowski1213@gmail.com>
2022-10-17 21:37:32 +0200

    add next components & styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Hero from './components/Hero/Hero'
 import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List';
-import Column from './components/Column/Column'
+import Column from './components/Column/Column';
+import Container from './components/Container/Container';
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Hero />
       <SearchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,13 +2,14 @@ import styles from './SearchForm.module.scss';
 
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
-import Column from '../Column/Column'
 
 const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
             <TextInput placeholder="Search..." />
-            <Button text="Search..." />
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
         </form>
     );
   };
